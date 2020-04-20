3040458690c11185a99adb62398348638ea63d0c
f0bs <fpdaiber@gmail.com>
2020-04-20 15:38:17 -0400

    Added TODOs

--- Project4_Stereo/student.py
+++ Project4_Stereo/student.py
@@ -29,7 +29,42 @@ def compute_photometric_stereo_impl(lights, images):
         normals -- float32 height x width x 3 image with dimensions matching
                    the input images.
     """
-    raise NotImplementedError()
+    # Create I a vector of N images from the list images by reshaping such
+    # that each image is an array of size 1980*1080*c with c=3 for RGB or 1 for greyscale
+    images_shape = np.shape(images)
+    I = np.array(images).reshape(images_shape[0], np.prod(images_shape[1:]))
+
+    # Compute the product of L transpose and L; then get it inverse 
+    L_t_L_inv = np.linalg.inv(np.dot(lights.T, lights))
+
+    # Compute the product of L transpose and I 
+    L_t_I = np.dot(lights.T, I)
+
+    # Compute G
+    G = np.dot(L_t_L_inv, L_t_I)
+
+    # Albedo
+    # Reshape the image back from an array of 1980x1080xc to an array of 3 dimensions
+    # Get the norm
+    G_shape = [G.shape[0]]
+    G_shape.extend(images_shape[1:])
+    G_albedo = G.reshape(G_shape)
+    albedo = np.linalg.norm(G_albedo, axis=0)
+
+    # Normals
+    # Use the norm after getting an average on all the colors and divide the norm
+    # by G
+    G_shape = []
+    G_shape.extend(images_shape[1:])
+    G_shape.append(3)
+    G_normal = np.mean(G.T.reshape(G_shape), axis=2)
+    albedo_normal = np.linalg.norm(G_normal, axis=2)
+    threshold = 1e-7
+    G_normal = G_normal/(1.0*np.maximum(threshold, albedo_normal[:,:,None]))
+
+    # There may be NaN values due to a division by zero
+    normals = np.nan_to_num(G_normal)
+    return albedo, normals
 
 
 def project_impl(K, Rt, points):
@@ -42,7 +77,28 @@ def project_impl(K, Rt, points):
     Output:
         projections -- height x width x 2 array of 2D projections
     """
-    raise NotImplementedError()
+    # Create projection matrix
+    projection_Matrix = np.dot(K, Rt)
+
+    # Switch from 3D coordinates to homogeneous coordinates
+    extra_ones = np.tile([1], points.shape[0]*points.shape[1])
+    extra_ones = extra_ones.reshape((points.shape[0], points.shape[1],1))
+    h_points = np.concatenate((points, extra_ones), axis=2)
+    X = np.reshape(h_points, (np.prod(h_points.shape[:2]), h_points.shape[2])).T
+    xs  = np.dot(projection_Matrix, X)
+    # Normalize to convert back from homogeneous coordinates to 2D coordinates
+    deno = xs[:,:,2][:,:,np.newaxis]
+    normalized_xs = xs / deno
+
+    return normalized_xs[:,:,:2]
+
+def get_pixel_values(img, i, j, u, v, isGrayscale):
+    """
+    Helper function to return the pixel values based on the position of the window
+    """
+    if i+u < 0 or i+u >= img.shape[0] or j+v < 0 or j+v >= img.shape[1]:
+        return 0 if isGrayscale else np.zeros(img.shape[2])
+    return img[i+u][j+v]
 
 
 def preprocess_ncc_impl(image, ncc_size):
@@ -94,7 +150,23 @@ def preprocess_ncc_impl(image, ncc_size):
     Output:
         normalized -- heigth x width x (channels * ncc_size**2) array
     """
-    raise NotImplementedError()
+    num_channels = image.shape[-1]
+    normalized = np.zeros((image.shape[0], image.shape[1], num_channels * ncc_size**2), dtype = np.float32)
+    mid_kernel = ncc_size//2
+    height, width, channel = image.shape
+
+    for i in range(mid_kernel, height - mid_kernel):
+        for j in range(mid_kernel, width - mid_kernel):
+            windows = []
+            for u in range(channel):
+                window = image[i - mid_kernel : i + mid_kernel + 1, j - mid_kernel : j + mid_kernel + 1, u]
+                window = (window - np.mean(window)).flatten()
+                windows.append(window.T)
+            flatten = np.array(windows).flatten()
+            norm = np.linalg.norm(flatten)
+            flatten = flatten / norm if norm > 1e-6 else np.zeros(flatten.shape)
+            normalized[i,j] = flatten
+    return normalized
 
 
 def compute_ncc_impl(image1, image2):
@@ -109,4 +181,5 @@ def compute_ncc_impl(image1, image2):
         ncc -- height x width normalized cross correlation between image1 and
                image2.
     """
-    raise NotImplementedError()
+    ncc = np.sum(np.multiply(image1, image2), axis = 2)
+    return ncc
